cff21ad6ca56a682d48796bf3e31cba0b6dbf5ee
deforas <BvarmTv2U>
2017-10-30 09:20:45 +0300

    someUPD

---
+++ client/projects/projects.js
@@ -0,0 +1,17 @@
+var projectData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.projectList.helpers({
+  project: projectData
+});
